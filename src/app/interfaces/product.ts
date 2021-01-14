33101a803c857e41d6723349cf0d5721570eb69a
picaporte-design <emendez.kroser@gmail.com>
2021-01-14 10:55:30 -0300

    corrección no mostrar inactivos

--- src/app/interfaces/product.ts
+++ src/app/interfaces/product.ts
@@ -19,6 +19,7 @@ export class Product {
     pictures?:string;
     visits?:number;
     pivot?: Pivot;
+    status?: string;
 }
 
 export interface Category {
@@ -44,4 +45,19 @@ export interface Pivot {
     price:number;
     product_id:number;
     quantity:number
+}
+
+export interface Paginate {
+    current_page: number;
+    data: any[];
+    first_page_url: string;
+    from?: any;
+    last_page: number;
+    last_page_url: string;
+    next_page_url?: any;
+    path: string;
+    per_page: number;
+    prev_page_url?: any;
+    to?: any;
+    total: number;
 }
--- src/app/modules/products/services/product.service.ts
+++ src/app/modules/products/services/product.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Product } from 'src/app/interfaces/product';
+import { Paginate, Product } from 'src/app/interfaces/product';
 import { environment } from 'src/environments/environment';
-import { take } from 'rxjs/operators';
+import { filter, map, take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -24,34 +24,54 @@ export class ProductService {
   }
   public getProduct(id): Observable<Product> {
     return this._http.get<Product>(`${environment.API}product/${id}`).pipe(
-      take(1)
+        take(1)
+      )
+    
+  }
+
+
+  public getProductByBrand(brand) : Observable<Product[]>{
+    return this._http.get<Product[]>(`${environment.API}brand-by-slug/${brand}`).pipe(
+      map( x => x.filter( v=> v.status !== "DIS"))
     )
   }
 
+  public getProductByCategory(category) : Observable<Product[]> {
 
-  public getProductByBrand(brand){
-    return this._http.get<Product[]>(`${environment.API}brand-by-slug/${brand}`)
+    return this._http.get<Product[]>(`${environment.API}category-by-slug/${category}`).pipe(
+      map( x => x.filter( v=> v.status !== "DIS"))
+    )
   }
 
-  public getProductByCategory(category) {
+  public getProductByCategoryPaginate(category,page)  : Observable<Paginate> {
 
-    return this._http.get<Product[]>(`${environment.API}category-by-slug/${category}`)
-  }
-
-  public getProductByCategoryPaginate(category,page) {
-
-    return this._http.get<Product[]>(`${environment.API}product-by-category/${category}?page=${page}`)
+    return this._http.get<Paginate>(`${environment.API}product-by-category/${category}?page=${page}`).pipe(
+      map( x => {
+        x.data.filter( v=> v.status !== "DIS")
+        return x
+      })
+    )
   }
   
-  public getProductByBrandPaginate(brand,page) {
+  public getProductByBrandPaginate(brand,page) : Observable<Paginate> {
 
-    return this._http.get<Product[]>(`${environment.API}product-by-brand/${brand}?page=${page}`)
+    return this._http.get<Paginate>(`${environment.API}product-by-brand/${brand}?page=${page}`).pipe(
+      map( x => {
+        x.data.filter( v=> v.status !== "DIS")
+        return x
+      })
+    )
   }
 
   //////SEARCHER/////
-  public searchProduct(data: any, page) {
+  public searchProduct(data: any, page) : Observable<Paginate> {
     
-    return this._http.get<Product[]>(`${environment.API}search-paginate/${data}?page=${page}`)
+    return this._http.get<Paginate>(`${environment.API}search-paginate/${data}?page=${page}`).pipe(
+      map( x => {
+        x.data.filter( v=> v.status !== "DIS")
+        return x
+      })
+    )
     // return this.products.find(product=> product.id === id);
 
     // return this.httpClient.get<Product>(this._url + 'product-' + id + '.json');
